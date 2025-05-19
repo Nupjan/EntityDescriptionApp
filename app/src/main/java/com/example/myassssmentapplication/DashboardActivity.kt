f43adf0909802eac6e795b6f48b7d01f5a5e5e74
Nupjan <nupjan.5koirala@gmail.com>
2025-05-19 21:55:16 +1000

    navigation to details page added

--- app/src/main/java/com/example/myassssmentapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/myassssmentapplication/DashboardActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myassssmentapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
@@ -15,6 +16,7 @@ class DashboardActivity : AppCompatActivity() {
     private lateinit var listView: ListView
     private lateinit var apiService: ApiService
     private lateinit var keypass: String
+    private lateinit var entitiesList: List<JsonObject>  // to keep entities for click
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,21 +42,30 @@ class DashboardActivity : AppCompatActivity() {
                     val json = response.body()!!
                     val entities = json.getAsJsonArray("entities")
 
+                    // Save entities for use on click
+                    entitiesList = entities.map { it.asJsonObject }
+
                     // Extract title and summary for each entity
                     val listItems = mutableListOf<String>()
-                    for (entityElement in entities) {
-                        val entity = entityElement.asJsonObject
-
+                    for (entity in entitiesList) {
                         // Get first two properties for title and summary as example
                         val title = entity.entrySet().firstOrNull()?.value?.asString ?: "No Title"
                         val summary = entity.entrySet().drop(1).firstOrNull()?.value?.asString ?: "No Summary"
-
                         listItems.add("$title\n$summary")
                     }
 
                     val adapter = ArrayAdapter(this@DashboardActivity, android.R.layout.simple_list_item_1, listItems)
                     listView.adapter = adapter
-                    // TODO: Add onItemClickListener to open details activity
+
+                    // Set click listener to open DetailsActivity
+                    listView.setOnItemClickListener { parent, view, position, id ->
+                        val selectedEntity = entitiesList[position]
+                        val entityJsonString = selectedEntity.toString()
+
+                        val intent = Intent(this@DashboardActivity, DetailsActivity::class.java)
+                        intent.putExtra("entity_data", entityJsonString)
+                        startActivity(intent)
+                    }
                 } else {
                     Toast.makeText(this@DashboardActivity, "Failed to load data", Toast.LENGTH_SHORT).show()
                 }
